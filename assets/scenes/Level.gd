436225f2360eba65a990e7beb091c665fd90803b
Tino Göhlert <tino.goehlert@lovoo.com>
2021-01-30 01:18:07 +0100

    fix level

--- assets/scenes/Level.gd
+++ assets/scenes/Level.gd
@@ -1,35 +1,51 @@
 extends Node2D
 
 export(Color, RGB) var clear_color
-onready var respawn_position = $Trigger/Respawns/R1.position
+onready var current_room = $YSort/Rooms/Room1
+onready var current_respawn = $YSort/Rooms/Room1/Respawns/R1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	VisualServer.set_default_clear_color(clear_color)
-	print(_get_camera_center())
 	pass # Replace with function body.
 
-func _get_camera_center():
-	var vtrans = get_canvas_transform()
-	var top_left = -vtrans.get_origin() / vtrans.get_scale()
-	var vsize = get_viewport_rect().size
-	return top_left + 0.5*vsize/vtrans.get_scale()
-
-func _on_R1_right_body_entered(body: Node) -> void:
-	if body is Player:
-		if $YSort/Room1.current:
-			respawn_position = $Trigger/Respawns/R2.position
-			$YSort/Room2.current = true
-
-		elif $YSort/Room2.current:
-			respawn_position = $Trigger/Respawns/R1.position
-			$YSort/Room1.current = true
-
-
 func _on_Player_dead() -> void:
-	$YSort/Player.position = respawn_position
-
+	$YSort/Player.position = current_respawn.position
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body is Player:
 		$Monolog.play("bottomless")
+
+func _find_nearest_respawn() -> void:
+	var last_distance: float = 1000000
+	var nearest_respawn = null
+	for respawn in current_room.get_node("Respawns").get_children():
+		var distance = respawn.get_position().distance_to($YSort/Player.position)
+		if distance < last_distance:
+			nearest_respawn = respawn
+			last_distance = distance
+
+		if nearest_respawn != current_respawn && nearest_respawn != null:
+			current_respawn = nearest_respawn
+
+func _on_Roomchecker_timeout() -> void:
+	var last_distance: float = 1000000
+	var nearest_room = null
+	for room in $YSort/Rooms.get_children():
+		var distance = room.get_position().distance_to($YSort/Player.position)
+		if distance < last_distance:
+			nearest_room = room
+			last_distance = distance
+
+	if nearest_room != current_room && nearest_room != null:
+		current_room = nearest_room
+		nearest_room.current = true
+		_find_nearest_respawn()
+
+
+
+func _on_MoodChange_body_entered(body: Node) -> void:
+	if body is Player:
+		var c = Color("#e16e1a")
+		$CanvasModulate.set_color(c)
