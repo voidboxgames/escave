cfa1f6dfb062ee92dddf33cd315623543df36132
Tino Göhlert <tino.goehlert@lovoo.com>
2021-01-30 16:41:19 +0100

    better monolog

--- assets/scenes/effects/Monolog.gd
+++ assets/scenes/effects/Monolog.gd
@@ -1,8 +1,32 @@
 extends CanvasLayer
 
 var done: Dictionary
+var monologues: Dictionary = {
+	"no_jump": "my heart feels so heavy.. i can't even [wave]jump[/wave]",
+	"can_jump": "get yourself together! you must try to [rainbow]jump[/rainbow]",
+	
+	"bottomless": "I'm slowly sinking into a [rainbow]bottomless[/rainbow] pit",
+	"go_on": "why should i even [rainbow]go on[/rainbow]?",
+	"the_fall": "it is too deep, i can't make it",
+	"agony": "there is just more [rainbow]agony[/rainbow] to come, i should give up...",
+}
+
+func visible(val: bool) -> void:
+	$TextureRect.visible = val
+	
+func _ready() -> void:
+	visible(false)
+	pause_mode = PAUSE_MODE_PROCESS
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("jump"):
+		get_tree().paused = false
+		$TextureRect/Label.bbcode_text = ""
+		visible(false)
 
 func play(monolog: String) -> void:
 	if !done.has(monolog):
-		$Animations.play(monolog)
+		get_tree().paused = true
+		$TextureRect/Label.bbcode_text = "[center]"+monologues[monolog]
 		done[monolog] = true
+		visible(true)
