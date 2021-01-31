6ed208b331df39863d4a6a045b27b914f9ee5c24
Tino Göhlert <tino.goehlert@lovoo.com>
2021-01-31 01:26:15 +0100

    fix bridge delete

--- assets/scenes/Trigger.gd
+++ assets/scenes/Trigger.gd
@@ -13,4 +13,4 @@ func _on_Area2D_body_shape_entered(body_id: int, body: Node, body_shape: int, ar
 
 func _on_Bridge1Trigger2_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
 	if body is Player:
-		get_parent().get_node("Bridges/Bridge").queue_free()
+		get_parent().get_node("Bridges/Bridge2").queue_free()
