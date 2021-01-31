dc63a32a7538d6acfb653da0b355e5d9ddd2cce7
Tino Göhlert <tino.goehlert@lovoo.com>
2021-01-31 01:23:05 +0100

    fix fireball, more triggers

--- assets/scenes/Trigger.gd
+++ assets/scenes/Trigger.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 func _on_Area2D_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
 	if body is Player:
 		get_parent().get_node("Bridges/Bridge").queue_free()
+
+
+func _on_Bridge1Trigger2_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
+	if body is Player:
+		get_parent().get_node("Bridges/Bridge").queue_free()
