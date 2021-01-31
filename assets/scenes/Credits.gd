92ada2ac22e3f59c7943f7b20ba5597ef9d469a2
Tino Göhlert <tino.goehlert@lovoo.com>
2021-01-31 02:03:47 +0100

    add credits

---
+++ assets/scenes/Credits.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var current_anim = 1
+var max_anim = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _unhandled_input(event: InputEvent) -> void:
+	if current_anim == max_anim: 
+		get_tree().change_scene("res://assets/scenes/Intro.tscn")
+
+func next() -> void:
+	if current_anim != max_anim:
+		current_anim += 1
+	$AnimationPlayer.play("a"+current_anim as String)
