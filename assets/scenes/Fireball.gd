ba35647952588c7069758ff7a0ea1084d1e6d825
Stan Domula <stan.domula@lovoo.com>
2021-02-02 00:27:31 +0100

    fix fireball timer 0

--- assets/scenes/Fireball.gd
+++ assets/scenes/Fireball.gd
@@ -1,27 +1,23 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
 export var startTime = 1.0
 
-# Called when the node enters the scene tree for the first time.
+onready var timer = $Timer
+onready var animation_plaer = $AnimationPlayer
+
 func _ready() -> void:
-	$Timer.wait_time = startTime
-	$Timer.start()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
+	if startTime == 0:
+		_start_fireball()
+		return
+	timer.wait_time = startTime
+	timer.start()
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body is Player:
 		body.die()
 
-
 func _on_Timer_timeout() -> void:
-	$AnimationPlayer.play("fly")
+	_start_fireball()
+
+func _start_fireball() -> void:
+	animation_plaer.play("fly")
